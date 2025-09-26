850287d44dcc9637df5874c4ed10b597eface11e
Gabriel Rodrigues <gabrielrns1997@gmail.com>
2025-09-26 12:53:34 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,79 +1,62 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){ 
+int main(){
 
-    char letra_estado1, letra_estado2;
-    char cod_carta1 [5], cod_carta2[5];
-    char nome_cidade1 [50], nome_cidade2 [50];
-    int populacao1, populacao2;
-    int pontos_turisticos1, pontos_turisticos2;
-    float area_km1, area_km2;
+    char nome_carta1, nome_carta2;
+    char estado1[50], estado2[50];
+    char cidade1[50], cidade2[50];
+    float quantidade_pop1, quantidade_pop2;
+    float areakm²1, areakm²2;
     float pib1, pib2;
-    float densidade_popu1, densidade_popu2;
-    float pib_percapita1, pib_percapita2;
-   
+    int pontoturis1, pontoturis2;
 
-    printf("Digite as informacões da carta número 1\n");
+    printf("Digite o nome da carta:\n");
+    scanf("%s", &nome_carta1);
 
-    printf("Digite uma letra: \n");
-    scanf("%c", &letra_estado1);
+    printf("Digite o nome do Estado:\n");
+    scanf("%s", &estado1);
 
-    printf("código da carta: \n");
-    scanf("%s", cod_carta1);
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", &cidade1);
 
-    printf("Nome da cidade: \n");
-    scanf("%s", nome_cidade1);
+    printf("Digite a aera em km²:\n");
+    scanf("%f", &areakm²1),
 
-    printf("Quantidade da população: \n");
-    scanf("%d", &populacao1);
-
-    printf("Área em KM²: \n");
-    scanf("%f", &area_km1);
-
-    printf("Digite o PIB: \n");
+    printf("Digite o pib:\n");
     scanf("%f", &pib1);
 
-    printf("Pontos turísticos: \n");
-    scanf("%d", &pontos_turisticos1);
+    printf("Digite quantos pontos turisticos:\n");
+    scanf("%d", &pontoturis1);
 
-   
-    printf("Digite as informacões da carta número 2\n");
 
-    printf("Digite uma letra: \n");
-    scanf(" %c", &letra_estado2);
+    printf("Digite o nome da carta:\n");
+    scanf("%s", &nome_carta2);
 
-    printf("código da carta: \n");
-    scanf("%s", cod_carta2);
+    printf("Digite o nome do Estado:\n");
+    scanf("%s", &estado2);
 
-    printf("Nome da cidade: \n");
-    scanf(" %s", nome_cidade2);
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", &cidade2);
 
-    printf("Quantidade da população: \n");
-    scanf("%d", &populacao2);
+    printf("Digite a aera em km²:\n");
+    scanf("%f", &areakm²2),
 
-    printf("Área em KM²: \n");
-    scanf("%f", &area_km2);
-
-    printf("Digite o PIB: \n");
+    printf("Digite o pib:\n");
     scanf("%f", &pib2);
 
-    printf("Pontos turísticos: \n");
-    scanf("%d", &pontos_turisticos2);
+    printf("Digite quantos pontos turisticos:\n");
+    scanf("%d", &pontoturis2);
 
 
-    
-    if (pontos_turisticos1 > pontos_turisticos2){
-        printf("Vencedor: Carta número 1:\n%s é a vencedora!\n", nome_cidade1);
+    if (pontoturis1 > pontoturis2){
+        printf("Vencedor: Carta número 1:\n%s é a vencedora!\n", cidade1);
     } 
-    else if (pontos_turisticos2 > pontos_turisticos1){
-        printf("Vencedor: Carta numero 2:\n%s é a vencedora!\n", nome_cidade2);
+    else if (pontoturis2 > pontoturis1){
+        printf("Vencedor: Carta numero 2:\n%s é a vencedora!\n", cidade2);
     } else {
         printf("Empate! Nenhuma carta vence!\n");
     }
 
-    
+    return 0;
 
-
-   
-return 0;
 }
